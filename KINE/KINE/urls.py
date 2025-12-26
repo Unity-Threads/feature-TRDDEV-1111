cadab90c7d82113f41511987f55bdc5dc3c24819
Navadeep3010 <navadeepm@techno-forest.com>
2025-12-26 15:36:06 +0530

    first commit

---
+++ KINE/KINE/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # Cart pages first
+    path('cart/', include(('cartPage.urls', 'cartPage'), namespace='cartPage')),
+
+    # Landing & base pages
+    path('', include('landing.urls')),
+    path('base/', include('base.urls')),
+
+    path('app/', include('app.urls')),
+    path('wishlist/', include('wishlist.urls')),
+    path('address/', include(('address.urls', 'address'), namespace='address')),
+    path('profile/', include('profile.urls')),
+    path('orders/', include('orders.urls')),
+]
