4edfa6e73d967e2629c041be10ee75198fe27bfc
sri-9893 <sreeja.bommana1123@gmail.com>
2026-01-02 16:37:11 +1400

    Delete unity-threads-core-dev-2025-12/KINE directory

--- unity-threads-core-dev-2025-12/KINE/address/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Address  # remove Order for now
-
-admin.site.register(Address)
-
-
--- unity-threads-core-dev-2025-12/KINE/landing/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Category
-
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'is_enabled', 'order')
-    list_editable = ('is_enabled', 'order')
-    ordering = ('order',)
